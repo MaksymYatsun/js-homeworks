899d580ee4f67e45b705304e556492f52f264d62
MaksymYatsun <egocogitio3.14@gmail.com>
2023-01-16 20:32:13 +0200

    File and function in it renamed. Declaring result variable was changed to . Return of the function was simplified. Code style fixes

--- homework__4/filterByLength.js
+++ homework__4/filterByLength.js
@@ -1,17 +1,16 @@
-function strOfGivenLength(arr, strLength = 5) {
-  let result = [];
+function filterByLength(arr, strLength = 5) {
+  const result = [];
+
   for (i = 0; i < arr.length; i++) {
     if (arr[i].length <= strLength) {
       result.push(arr[i])
     }
   }
-  if (result.length !== 0) {
-    return result
-  }
-  return false
+
+  return result;
 }
 
-console.log(strOfGivenLength(['test', 'delegation', 'forward', 'bliss', 'hop'], 4));
-console.log(strOfGivenLength(['haste', 'hazelnut', 'hat', 'hacker', 'hiking']));
+console.log(filterByLength(['test', 'delegation', 'forward', 'bliss', 'hop'], 4));
+console.log(filterByLength(['haste', 'hazelnut', 'hat', 'hacker', 'hiking']));
 
 //Write a function that accepts an array of strings and an optional number (default value is 5). This function should return a new array containing strings with length <= the given number

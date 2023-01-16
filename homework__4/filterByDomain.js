3e2c89ee931aedb434c2bf5f01b30cb2f4282923
MaksymYatsun <egocogitio3.14@gmail.com>
2023-01-16 20:34:18 +0200

    File and function in it renamed. Declaring result variable was changed to . Return of the function was simplified. Code style fixes

---
+++ homework__4/filterByDomain.js
@@ -0,0 +1,15 @@
+function filterByDomain(arr, domain) {
+  const result = [];
+
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i].endsWith(domain)) {
+      result.push(arr[i])
+    }
+  }
+
+  return result
+}
+
+console.log(filterByDomain(['steve@yahoo.com', 'jack@google.com', 'kate@mailbox.com', 'luke@google.com'], "@google.com"))
+
+// 3. Write a function that accepts an array of emails and a domain name. Return a new array containing emails with the given domain name.

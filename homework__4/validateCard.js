d4adf5ecca224ac69bb76adc7f3a8ab2c8bc1a7b
MaksymYatsun <egocogitio3.14@gmail.com>
2023-01-17 23:30:39 +0200

    Small refactoring.  mode enabled. Code style fixes

--- homework__4/validateCard.js
+++ homework__4/validateCard.js
@@ -1,24 +1,29 @@
-function validateCard(cardNum) {
+'use strict'
+
+function validateCard2(cardNum) {
   const cardArr = cardNum.split('');
   const checkDigit = parseInt(cardArr.pop());
-  let cardSum = 0;
+  let cardDigitSum = 0;
 
-  for (i = 0; i < cardArr.length; i++) {
-    cardArr[i] = parseInt(cardArr[i])
+  for (let i = 0; i < cardArr.length; i++) {
+    cardArr[i] = parseInt(cardArr[i]);
     if (i % 2 === 0) {
       cardArr[i] *= 2;
-      if (cardArr[i] >= 10) {
-        cardArr[i] -= 9;
-      }
     }
-    cardSum += cardArr[i]
   }
 
-  return 0 === (checkDigit + cardSum) % 10
+  for (let i = 0; i < cardArr.length; i++) {
+    if (cardArr[i] >= 10) {
+      cardArr[i] -= 9;
+    }
+    cardDigitSum += cardArr[i];
+  }
+
+  return (cardDigitSum + checkDigit) % 10 === 0;
 }
 
-console.log(validateCard('4441114414052504')) // RealCard
-console.log(validateCard('4149609014545778')) // RealCard
-console.log(validateCard('8881147414052364')) // FakeCard
+console.log(validateCard2('4441114414052504')) // RealCard
+console.log(validateCard2('4149609014545778')) // RealCard
+console.log(validateCard2('8881147414052364')) // FakeCard
 
 // 1 .Write a function that takes a credit card number (string) and returns boolean whether the number is valid. Use the Lunh algorithm to validate the number.

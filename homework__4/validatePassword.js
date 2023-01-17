8b33640cc35a8803f7a2448fffe4f096912e78c0
MaksymYatsun <egocogitio3.14@gmail.com>
2023-01-17 19:05:17 +0200

    Two conditions is added to separate functions.  mode added, code style fixed

--- homework__4/validatePassword.js
+++ homework__4/validatePassword.js
@@ -1,25 +1,38 @@
-function validatePassword(password) {
-  if (password.length < 8 || password.length > 30) {
+'use strict'
 
-    return false
-  } else if (password.toLowerCase() === password) {
-
-    return false
-  } else if (password.includes('%') || password.includes('?') || password.includes('!') || password.includes('*') || password.includes('#')) {
-
-    return false
-  }
-
-  for (i = 0; i < password.length; i++) {
-    if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57) {
-
-      return true
+function includesSymbols(password) {
+  for (let i = 0; i < password.length; i++) {
+    if (password.charCodeAt(i) === (33 || 35 || 37 || 63)) {
+      return true;
     }
   }
+
+  return false;
+}
+
+function includesNumbers(password) {
+  for (let i = 0; i < password.length; i++) {
+    if (password.charCodeAt(i) >= 48 && password.charCodeAt(i) <= 57) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function validatePassword(password) {
+  if (password.length < 8 || password.length > 30) {
+    return false;
+  }
+  if (password.toLowerCase() === password) {
+    return false;
+  }
+
+  return includesNumbers(password) && !includesSymbols(password);
 }
 
 console.log(validatePassword('Str0ngpassw0rd'));
 console.log(validatePassword('justabiggerpassword1'));
-console.log(validatePassword('He1llo World!'));
+console.log(validatePassword('Hello World!'));
 
 // 4. Write a function that accepts a password and returns boolean whether the password is valid.

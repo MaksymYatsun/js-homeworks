1534f7b25d0b662bb10fb4d3114127b8a068e47c
MaksymYatsun <egocogitio3.14@gmail.com>
2023-01-25 23:14:10 +0200

    Full refactoring

--- homework__4/stringSplitter.js
+++ homework__4/stringSplitter.js
@@ -1,33 +1,53 @@
 'use strict'
 
-function stringSplitter(str, del = '') {
+function stringSplitter(str, delimiter = '') {
   const arr = [];
-  let delLength = del.length;
+  let tmpStr = '';
+  let isDelimiter = false;
+
+  if (delimiter.length === 0) {
+
+    for (let i = 0; i < str.length; i++) {
+      arr.push(str[i]);
+    }
+
+    return arr;
+  }
 
   for (let i = 0; i < str.length; i++) {
-    if (str.substr(i, delLength) === del) {
-      if (str.substr(0, i).length !== 0) {
-        arr.push(str.substr(0, i));
+    if (str[i] === delimiter[0]) {
+
+      for (let j = 0; j < delimiter.length; j++) {
+        if (str[i + j] === delimiter[j]) {
+          isDelimiter = true;
+        }
+
+        if (str[i + j] !== delimiter[j]) {
+          isDelimiter = false;
+        }
       }
     }
-  }
-  arr.push(str.substr(0, str.length));
 
-  for (let i = arr.length - 1; i > 0; i--) {
-    let startPosition = delLength + arr[i - 1].length;
-    arr[i] = arr[i].substr(startPosition);
-    if (arr[i].length === 0) {
-      arr.splice(i, 1);
+    if (isDelimiter === false) {
+      tmpStr += str[i];
+    }
+
+    if (isDelimiter) {
+      arr.push(tmpStr);
+      tmpStr = '';
+      isDelimiter = false;
+      i += delimiter.length - 1;
     }
   }
+  arr.push(tmpStr);
 
   return arr;
 }
 
-console.log(stringSplitter('Jake, Jim, Kate, John', ', '))
-console.log(stringSplitter('ABCXYZDHFXYTXYZBLA', 'XYZ'))
-console.log(stringSplitter('Hello'))
-console.log(stringSplitter('TEST1-*--TEST2-*-*--TEST3-**-TEST4', '-*-'))
+console.log(stringSplitter('Jake, Jim, Kate, John', ', '));
+console.log(stringSplitter('ABCXYZDHFXYTXYZBLA', 'XYZ'));
+console.log(stringSplitter('Hello'));
+console.log(stringSplitter('TEST1-*--TEST2-*-*--TEST3-**-TEST4', '-*-'));
 
 /* 
 Write a simplified version of String.split() function. It should accept a string and a delimiter (default value is ''). 
